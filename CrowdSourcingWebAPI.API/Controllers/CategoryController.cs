c463cc92aca3efc859902d333d1a57cfca36a5b4
internitys <aminearbi@live.com>
2015-02-21 18:11:59 +0100

    Changing webap to CrowdSourcingWebAPI.API

---
+++ CrowdSourcingWebAPI.API/Controllers/CategoryController.cs
@@ -0,0 +1,84 @@
+﻿using CrowdSourcingWebAPI.Domain.Entities;
+using CrowdSourcingWebAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrowdSourcingWebAPI.API.Controllers
+{
+    [RoutePrefix ("api/Category")]
+    public class CategoryController : ApiController
+    {
+    CrowdService service;
+    public CategoryController ()
+        {
+        service = new CrowdService ();
+
+        }
+    protected override void Dispose ( bool disposing )
+        {
+        if (disposing)
+            {
+            service.Dispose ();
+            }
+        base.Dispose (disposing);
+        }
+    // GET: api/Category
+    [Route ("GetByTenant/{tenant}/")]
+    public IEnumerable<Category> GetByTenant ( string tenant )
+        {
+        return service.GetCategoriesByTenant (tenant);
+        }
+
+    [Route ("GetById/{id}/")]
+    public Category GetCategoryById ( int id )
+        {
+        Category c = service.GetCategoryById (id);
+        return c;
+        }
+
+    // POST: api/Category
+    [HttpPost]
+    public HttpResponseMessage PostCategory ( Category c )
+        {
+        if (c.Title != null)
+            {
+            if (service.CheckCategory (c)==false)
+                {
+                service.CreateCategory (c);
+
+                }
+            return new HttpResponseMessage (HttpStatusCode.Accepted);
+
+            }
+        else
+            throw new HttpResponseException (HttpStatusCode.NoContent);
+        }
+
+    // PUT: api/Category/5
+    public void Put ( int id, [FromBody]string value )
+        {
+
+        }
+
+    // DELETE: api/Category/5
+    public HttpResponseMessage Delete ( int id )
+        {
+        if (id!=0)
+            {
+            Category c= service.GetCategoryById (id);
+            service.DeleteCategory (c);
+            return new HttpResponseMessage (HttpStatusCode.Accepted);
+            }
+        else
+            {
+            throw new HttpResponseException (HttpStatusCode.NoContent);
+            }
+
+
+        }
+    }
+}
---
+++ CrowdSourcingWebAPI.API/Controllers/LogController.cs
@@ -0,0 +1,91 @@
+﻿using CrowdSourcingWebAPI.Domain.Entities;
+using CrowdSourcingWebAPI.Service;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrowdSourcingWebAPI.API.Controllers
+{
+    public class LogController : ApiController
+    {
+
+    CrowdService service;
+    public LogController ()
+        {
+        service = new CrowdService ();
+        }
+    // GET: api/Log
+    /*
+    public HttpResponseException Get()
+    {
+        throw new HttpResponseException(HttpStatusCode.Forbidden);
+    }
+    */
+    // GET: api/Log/email
+    [HttpPost]
+    [Route ("api/log/email")]
+    public IEnumerable<Log> GetLogsForTenant ( JObject o )
+        {
+        string tenantemail = o["email"].ToObject<string> ();
+        if (tenantemail != null)
+            {
+            IEnumerable<Log> logs = service.GetLogByTenant (tenantemail);
+            return logs;
+
+            }
+        else throw new HttpResponseException (HttpStatusCode.NotFound);
+
+
+        }
+
+    // content must be sent as a json array with email attribute. Encoding MUST be set to text/plain
+    /*
+    [HttpPost]
+    [Route("api/log/email")]
+    public IEnumerable<Log> GetLogsForTenant(JObject o)
+    {
+        string tenantemail = o["email"].ToObject<string>();
+        if (tenantemail != null)
+        {
+            IEnumerable<Log> logs = service.GetLogByTenant(tenantemail);
+            return logs;
+
+        }
+        else throw new HttpResponseException(HttpStatusCode.NotFound);
+            
+
+    }
+    */
+
+
+    [HttpPost]
+
+    // POST: api/Log
+    public HttpResponseMessage Post ( Log log )
+        {
+        if (log != null)
+            {
+            service.CreateLog (log);
+            return new HttpResponseMessage (HttpStatusCode.Created);
+            }
+        else
+            throw new HttpResponseException (HttpStatusCode.NoContent);
+        }
+
+    // PUT: api/Log/5
+    public HttpResponseException Put ()
+        {
+        throw new HttpResponseException (HttpStatusCode.Forbidden);
+        }
+
+    // DELETE: api/Log/5
+    public HttpResponseException Delete ( int id )
+        {
+        throw new HttpResponseException (HttpStatusCode.Forbidden);
+        }
+    }
+}
